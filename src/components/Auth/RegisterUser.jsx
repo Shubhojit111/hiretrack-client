a1b9c1b6f0c5ba7f41f7480f53dd74b49afd7c72
Shubhojit Deb <shubhojityes@gmail.com>
2026-02-26 04:09:27 +0530

    Replaced localhost with serverURL2

--- src/components/Auth/RegisterUser.jsx
+++ src/components/Auth/RegisterUser.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
-import config from "../utils/config"
+import config from "../../utils/config"
 
 const RegisterUser = () => {
   const serverURL = config.serverURL;
