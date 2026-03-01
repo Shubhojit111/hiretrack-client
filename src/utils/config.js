1fc1d730fe8e76c9463aa2cd5312b43d883cb3d0
Shubhojit Deb <shubhojityes@gmail.com>
2026-03-01 09:55:24 +0530

    config.js client isLocalhost isssue fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,8 @@
+const isLocalhost =
+  typeof window !== "undefined" &&
+  (window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1");
+
 const config = {
   // Use local API during local development, fallback to deployed API otherwise
   serverURL: isLocalhost
